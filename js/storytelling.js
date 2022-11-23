fdb07df4c48ec25b2ffd0872fc445ff7cffdfeb9
Alex Guillard <a.w.s.guillard@gmail.com>
2022-11-23 13:36:35 +0100

    [UI][STORYTELLING] Change opacity of old narratives

--- js/storytelling.js
+++ js/storytelling.js
@@ -1,9 +1,16 @@
 const MAX_NARRATIVES = 10;
 
 export class Storytelling {
-  static addNarrative(narrative, node) {
-    node.insertAdjacentHTML("afterbegin", "<div class='narrative'>" + narrative + "</div>");
-    if (node.getElementsByClassName("narrative").length > MAX_NARRATIVES)
-      node.removeChild(node.lastChild);
+  static addNarrative(narrative, storytellingNode) {
+    storytellingNode.insertAdjacentHTML("afterbegin", "<div class='narrative'>" + narrative + "</div>");
+    const narrativeNodes = storytellingNode.getElementsByClassName("narrative");
+    if (narrativeNodes.length > MAX_NARRATIVES)
+      storytellingNode.removeChild(storytellingNode.lastChild);
+
+    let opacity = 1;
+    for (let node of narrativeNodes) {
+      node.style.opacity = opacity;
+      opacity -= 1 / MAX_NARRATIVES;
+    }
   }
 }
